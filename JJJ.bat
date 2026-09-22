1dc28a56d3c858b07e975c982aa2d15bc9fbc43b
eidanexdll <eidanexdll@gmail.com>
2026-09-21 20:59:12 -0500

    Update JJJ.bat

--- JJJ.bat
+++ JJJ.bat
@@ -1,2 +1,5 @@
-
-GODETE CHERRY na mentirita :3
+@echo off
+echo repositorio dll estado :expusto por CHerry
+echo repositorio A estado : indecifrable
+echo repositorio xxx estado : desconocido
+echo repositorio B estado : seguro temporal
